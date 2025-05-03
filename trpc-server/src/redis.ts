a19113f75864ea272f0414e407f9bb10425b5602
orkward <orkward_o@outlook.com>
2025-05-03 22:30:09 +0800

    [trpc] fix bad reference

--- trpc-server/src/redis.ts
+++ trpc-server/src/redis.ts
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 import { C } from "./config.js";
-import { logger } from "./util.js";
+import { logger } from "./logger.js";
 
 export const redis = await createClient({ url: C.REDIS })
   .on("connect", () => {
