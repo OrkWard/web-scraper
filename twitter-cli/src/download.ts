b4d1393795f1d6056d484e84439d2ac94d9aaf2b
orkward <orkward_o@outlook.com>
2025-07-25 19:08:04 +0800

    [twitter-cli] graceful shutdown

--- twitter-cli/src/download.ts
+++ twitter-cli/src/download.ts
@@ -2,56 +2,45 @@ import { writeFileSync } from "fs";
 import { writeFile } from "fs/promises";
 import * as path from "path";
 
-import { get, sleep } from "./utils.js";
+import { get, sleep, GracefulShutdown } from "./utils.js";
 
-function handleSIGINT(callback?: () => void) {
-  let aboutToExit = false;
-  process.on("SIGINT", () => {
-    callback?.();
-    aboutToExit = true;
-  });
-
-  return () => {
-    return aboutToExit;
-  };
-}
+const shutdown = new GracefulShutdown();
 
 export async function downloadAll(urls: string[], outputDir: string) {
-  /** download progress */
   let i = 0;
-  function saveProgress() {
-    console.log("|> Saving Progress...");
-    writeFileSync(path.join(outputDir, "download.int"), urls.slice(i).join("\n"));
-    console.log("|> About to exit...");
-  }
-
-  const existing = handleSIGINT(saveProgress);
 
   console.log(`|> Start download, remaining ${urls.length}`);
   await sleep();
 
-  for (; i < urls.length; i += 10) {
-    if (existing()) {
-      process.exit(0);
+  try {
+    for (; i < urls.length; i += 10) {
+      if (shutdown.isExiting()) {
+        console.log("Interrupted.");
+        throw new Error();
+      }
+
+      await Promise.all(
+        urls.slice(i, i + 10).map(async (u) => {
+          const file = await get(u).buffer();
+          if (!file) {
+            return;
+          }
+          const p = new URL(u).pathname;
+          const outputPath = path.join(outputDir, p.slice(p.lastIndexOf("/") + 1));
+
+          return writeFile(outputPath, file);
+        }),
+      ).catch((e) => {
+        console.error(e);
+        throw new Error();
+      });
+
+      console.log(`==> progress: ${(i + urls.slice(i, i + 10).length) / urls.length}`);
+      await sleep(500);
     }
-    await Promise.all(
-      urls.slice(i, i + 10).map(async (u) => {
-        const file = await get(u).buffer();
-        if (!file) {
-          return;
-        }
-        const p = new URL(u).pathname;
-        const outputPath = path.join(outputDir, p.slice(p.lastIndexOf("/") + 1));
-
-        return writeFile(outputPath, file);
-      }),
-    ).catch((e) => {
-      console.error(e);
-      saveProgress();
-      process.exit(0);
-    });
-
-    console.log(`  progress: ${(i + urls.slice(i, i + 10).length) / urls.length}`);
-    await sleep(500);
+  } catch {
+    console.log("|> Saving Progress...");
+    writeFileSync(path.join(outputDir, "download.int"), urls.slice(i).join("\n"));
+    console.log("|> About to exit...");
   }
 }
--- twitter-cli/src/utils.ts
+++ twitter-cli/src/utils.ts
@@ -12,5 +12,24 @@ export async function sleep(time: number = 1000) {
 const proxyEnv = process.env.https_proxy || process.env.all_rpoxy;
 const proxyAgent = proxyEnv ? new HttpsProxyAgent(proxyEnv) : undefined;
 
+// fetcher with proxy
 export const instance = got.extend({ agent: { https: proxyAgent } });
 export const { get } = instance;
+
+// shutdown detect
+export class GracefulShutdown {
+  private isShuttingDown = false;
+  private signalHandler = () => {
+    if (this.isShuttingDown) return;
+    this.isShuttingDown = true;
+  };
+
+  constructor() {
+    process.on("SIGINT", this.signalHandler);
+    process.on("SIGTERM", this.signalHandler);
+  }
+
+  isExiting(): boolean {
+    return this.isShuttingDown;
+  }
+}
